a38953a3a7d993f7bf38234cac66d49b0aa06f89
atalay11 <oatalay.w@gmail.com>
2023-07-30 01:02:59 +0300

    Remove unnecessary operator overload

--- Assets/Scripts/Board/LetterLocation.cs
+++ Assets/Scripts/Board/LetterLocation.cs
@@ -24,11 +24,6 @@ public struct LetterLocation
         return new LetterLocation(l.column - v.x, l.row - v.y);
     }
 
-    public static Vector2Int operator +(LetterLocation l1, LetterLocation l2)
-    {
-        return new Vector2Int(l1.column + l1.column, l2.row + l2.row);
-    }
-
     public static Vector2Int operator -(LetterLocation l1, LetterLocation l2)
     {
         return new Vector2Int(l1.column - l2.column, l1.row - l1.row);
