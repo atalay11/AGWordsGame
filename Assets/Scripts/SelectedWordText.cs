8d0e7ebbd920f6629badc9eb63fbf9ef1dd9ee7e
Enver Nuri <enverabc@gmail.com>
2023-07-30 14:00:03 +0300

    SelectedWordText does not remove the text on release.

--- Assets/Scripts/SelectedWordText.cs
+++ Assets/Scripts/SelectedWordText.cs
@@ -18,17 +18,11 @@ public class SelectedWordText : MonoBehaviour
     private void Start()
     {
         LetterSelectionChecker.Instance.OnSelectedWordChanged += LetterSelectionChecker_OnSelectedWordChanged;
-        GameInput.Instance.OnSelectReleaseAction += GameInput_OnSelectReleaseAction;
     }
 
     private void LetterSelectionChecker_OnSelectedWordChanged(object sender, LetterSelectionChecker.OnSelectedWordChangedEventArgs e)
     {
         tmpText.text = e.word;
-    }
-
-    private void GameInput_OnSelectReleaseAction(object sender, EventArgs e)
-    {
-        tmpText.text = "";
     }
 
 }
