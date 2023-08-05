96dab9d27db4fc5cba2098bc6f6876c259fe63cd
Enver Nuri <enverabc@gmail.com>
2023-08-05 17:34:15 +0300

    Add MainMenu script

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,17 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
-    const string mainMenuScene = "MainMenuScene";
-    const string playScene = "PlayScene";
-
     public void OnPlayButtonPressed()
     {
-        SceneManager.LoadScene(playScene, LoadSceneMode.Single);
+        SceneLoader.LoadPlayModesScene();
     }
 
 }
