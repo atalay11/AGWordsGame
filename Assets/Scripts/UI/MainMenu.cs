d09d45987a1289b0cac51528a0591ef36ecbb0b6
Enver Nuri Ünal <31821023+enverunal@users.noreply.github.com>
2023-09-16 15:00:01 +0300

    Add Settings menu, language selection, saving language selection to user preferences.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -12,18 +12,48 @@ public class MainMenu : GenericSingleton<MainMenu>
 
     public void OnSettingsButtonPressed()
     {
-        //TODO: Load Settings Scene
+        Debug.Log("OnSettingsButtonPressed");
+        mainMenuVisual.gameObject.SetActive(false);
+        settingsMenuVisual.gameObject.SetActive(true);
     }
 
     public void OnQuitButtonPressed()
     {
         // Check if the application is running in the Unity Editor (for testing)
 #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
+        UnityEditor.EditorApplication.isPlaying = false;
 #else
             // If not in the Unity Editor, quit the application
             Application.Quit();
 #endif
     }
+
+    // Settings Menu
+
+    public void OnLanguageSelectButtonPressed()
+    {
+        Debug.Log("OnLanguageSelectButtonPressed");
+        languageSelectionVisual.gameObject.SetActive(true);
+    }
+
+    public void OnSettingsMenuCloseButtonPressed()
+    {
+        Debug.Log("OnSettingsMenuClosed");
+        mainMenuVisual.gameObject.SetActive(true);
+        settingsMenuVisual.gameObject.SetActive(false);
+    }
+
+    public void OnLanguageSelected()
+    {
+        Debug.Log("OnLanguageSelected");
+        languageSelectionVisual.gameObject.SetActive(false);
+    }
+
+    // Variables
+
+    [SerializeField] private Transform mainMenuVisual;
+    [SerializeField] private Transform settingsMenuVisual;
+    [SerializeField] private Transform languageSelectionVisual;
+
 
 }
